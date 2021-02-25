13401e211a4b803d745274ac18ea65421dfe4183
jaehee <vito822@naver.com>
2021-02-26 01:03:45 +0900

    feat(Posting): add image/file input button

--- src/pages/Posting.js
+++ src/pages/Posting.js
@@ -2,16 +2,13 @@ import { React, useState } from "react";
 import MainLayout from "../components/MainLayout";
 import { makeStyles } from "@material-ui/core/styles";
 import { FormControl, Select, Button, TextField } from "@material-ui/core";
-import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined';
-//import Link from "../image/link.png";
+import CameraAltOutlinedIcon from "@material-ui/icons/CameraAltOutlined";
+import LinkIcon from "@material-ui/icons/Link";
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
+  select: {
     width: "340px",
     height: "56px",
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
   },
   postBtn: {
     position: "absolute",
@@ -25,10 +22,12 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "16px",
   },
   attachBtn: {
-    display: "inline-block",
     width: "160px",
     height: "56px",
-  }
+    fontSize: "16px",
+    lineHeight: "24px",
+    marginRight: "20px",
+  },
 }));
 
 const Posting = () => {
@@ -40,12 +39,33 @@ const Posting = () => {
     categoryName: "",
   });
 
-  const onChangeCategoryName = (e) => {
+  const onChangePost = (e) => {
     setPost({
       ...post,
-      categoryName: e.target.value,
+      [e.target.name]: e.target.value,
     });
+    //console.log(post);
   };
+
+  const onChangeImg = (e) => {
+    const formData = new FormData();
+    formData.append("img", e.target.files[0]);
+    console.log(formData);
+  };
+
+  const onChangeFile = (e) => {
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+  };
+
+  const onClickImageInput = () => {
+    document.getElementById("imageInput").click();
+  };
+
+  const onClickFileInput = () => {
+    document.getElementById("fileInput").click();
+  };
+
 
   const onPost = () => {
     if (confirm("등록하시겠습니까?")) {
@@ -70,10 +90,12 @@ const Posting = () => {
           <Select
             native
             value={post.categoryName}
-            onChange={onChangeCategoryName}
+            name="categoryName"
+            onChange={onChangePost}
             inputProps={{
               name: "categoryName",
             }}
+            className={classes.select}
           >
             <option value="" disabled>
               카테고리를 선택해주세요.
@@ -87,21 +109,53 @@ const Posting = () => {
           id="titleField"
           placeholder="제목을 입력해주세요."
           variant="outlined"
+          name="title"
+          onChange={onChangePost}
         />
         <TextField
           id="contentField"
           placeholder="내용을 입력해주세요."
           variant="outlined"
           rows={20}
+          name="content"
+          onChange={onChangePost}
           multiline
         />
-        <Button
-          variant="outlined"
-          endIcon={<CameraAltOutlinedIcon/>}
-          className={classes.attachBtn}
-        >
-          사진추가
-        </Button>
+        <div className="posting-attaches">
+          <input
+            accept="image/*"
+            className={classes.input}
+            style={{ display: "none" }}
+            onChange={onChangeImg}
+            multiple
+            id="imageInput"
+            type="file"
+          />
+          <Button
+            variant="outlined"
+            endIcon={<CameraAltOutlinedIcon />}
+            className={classes.attachBtn}
+            onClick={onClickImageInput}
+          >
+            사진추가
+          </Button>
+          <input
+            className={classes.input}
+            style={{ display: "none" }}
+            onChange={onChangeFile}
+            multiple
+            id="fileInput"
+            type="file"
+          />
+          <Button
+            variant="outlined"
+            endIcon={<LinkIcon />}
+            className={classes.attachBtn}
+            onClick={onClickFileInput}
+          >
+            첨부파일
+          </Button>
+        </div>
       </div>
     </MainLayout>
   );
