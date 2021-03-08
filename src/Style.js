fdb0fe3af6ae79b1b9b6e03fd6b65bd788d2164f
healing99 <nicealice99@gmail.com>
2021-03-09 02:32:58 +0900

    feat(Settings): add category settings

--- src/Style.js
+++ src/Style.js
@@ -32,7 +32,39 @@ const useStyles = makeStyles((theme) => ({
     height: "160px",
     marginBottom: "40px",
     padding: "16px",
-  }
+  },
+  table: {
+    width: "880px",
+    marginTop: "40px",
+    borderRadius: "8px",
+  },
+  mainRow: {
+    backgroundColor: "#F2F2F2",
+    border: "none",
+    paddingInline: 0,
+  },
+  subRow: {
+    border: "none",
+    paddingInline: 0,
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  cancelBtn: {
+    width: "70px",
+    height: "32px",
+    backgroundColor: "#ffffff",
+    boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.2)",
+    borderRadius: "8px",
+    marginLeft: "20px",
+  },
+  confirmBtn: {
+    width: "70px",
+    height: "32px",
+    backgroundColor: "#00462A",
+    boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.2)",
+    borderRadius: "8px",
+    marginLeft: "8px",
+  },
 }));
 
 export default useStyles;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-export { default as Home } from './Home';
-export { default as Post } from './Post';
-export { default as Posting } from './Posting';
-export { default as Category } from './Category'; // 한 카테고리 내
+export { default as Home } from "./Home";
+export { default as Post } from "./Post";
+export { default as Posting } from "./Posting";
+export { default as Category } from "./Category"; // 한 카테고리 내
+export { default as Settings } from "./Settings";
+
 /* 사용자 인증 */
-export { default as SignUp } from './Authentication/SignUp'; // 회원가입
-export { default as SignIn } from './Authentication/SignIn'; // 로그인
-export { default as FindID } from './Authentication/FindID'; // 아이디 찾기
-export { default as FindPW } from './Authentication/FindPW'; // 비밀번호 찾기
+export { default as SignUp } from "./Authentication/SignUp"; // 회원가입
+export { default as SignIn } from "./Authentication/SignIn"; // 로그인
+export { default as FindID } from "./Authentication/FindID"; // 아이디 찾기
+export { default as FindPW } from "./Authentication/FindPW"; // 비밀번호 찾기
